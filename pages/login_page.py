9c57a652b3e78f2205290d657481606268660cca
Gaarrs <callmeimmortal@gmail.com>
2026-07-24 02:37:03 +0300

    added new methods on LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,9 +4,22 @@ from base_page import BasePage
 class LoginPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
-        login_email = page.locator('[data-qa="login-email"]')
-        login_password = page.locator('[data-qa="login-password"]')
-        login_button = page.olcator('[data-qa="login-button"]')
-        signup_name = page.locator('[data-qa="signup-name"]')
-        signup_email = page.locator('[data-qa="signup-email"')
-        signup_button = page.locator('[data-qa="signup-button"]')
+        self.login_email = page.locator('[data-qa="login-email"]')
+        self.login_password = page.locator('[data-qa="login-password"]')
+        self.login_button = page.locator('[data-qa="login-button"]')
+        self.signup_name = page.locator('[data-qa="signup-name"]')
+        self.signup_email = page.locator('[data-qa="signup-email"')
+        self.signup_button = page.locator('[data-qa="signup-button"]')
+
+    def open(self):
+        LoginPage.navigate(self, "https://automationexercise.com/login")
+
+    def signup(self, name, email):
+        self.signup_name.fill(name)
+        self.signup_email.fill(email)
+        self.signup_button.click()
+
+    def login(self, email, password):
+        self.login_email.fill(email)
+        self.login_password.fill(password)
+        self.login_button.click()
