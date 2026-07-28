96bfa7f6de371d6ebc4e740ee14c80022aaa0557
Gaarrs <callmeimmortal@gmail.com>
2026-07-29 02:28:29 +0300

    signup_page locators added

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,11 +4,11 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
-        self.login_email = page.locator('[data-qa="login-email"]')
-        self.login_password = page.locator('[data-qa="login-password"]')
+        self.login_email_input = page.locator('[data-qa="login-email"]')
+        self.login_password_input = page.locator('[data-qa="login-password"]')
         self.login_button = page.locator('[data-qa="login-button"]')
-        self.signup_name = page.locator('[data-qa="signup-name"]')
-        self.signup_email = page.locator('[data-qa="signup-email"]')
+        self.signup_name_input = page.locator('[data-qa="signup-name"]')
+        self.signup_email_input = page.locator('[data-qa="signup-email"]')
         self.signup_button = page.locator('[data-qa="signup-button"]')
         self.signup_header = page.get_by_text("New User Signup!")
 
@@ -16,11 +16,11 @@ class LoginPage(BasePage):
         LoginPage.navigate(self, "https://automationexercise.com/login")
 
     def signup(self, name, email):
-        self.signup_name.fill(name)
-        self.signup_email.fill(email)
+        self.signup_name_input.fill(name)
+        self.signup_email_input.fill(email)
         self.signup_button.click()
 
     def login(self, email, password):
-        self.login_email.fill(email)
-        self.login_password.fill(password)
+        self.login_email_input.fill(email)
+        self.login_password_input.fill(password)
         self.login_button.click()
