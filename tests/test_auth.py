791242d5fbc6cc9f6af0e7a7167182b185fa7925
Gaarrs <callmeimmortal@gmail.com>
2026-08-04 02:49:52 +0300

    user logout test added

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -44,6 +44,25 @@ def test_user_registration(login_page, signup_page, acc_created_page, acc_delete
     #     acc_deleted_page.continue_button.click()
 
 @allure.story('Authentication feature')
+@allure.title("Logout User")
+def test_user_logout(login_page, page):
+    with allure.step("Открыть домашнюю страницу"):
+        login_page.navigate("https://automationexercise.com/")
+    with allure.step("Нажать на ссылку Signup/Login"):
+        login_page.login_link.click()
+    with allure.step("Проверить, что отображается заголовок 'Login to your account'"):
+        expect(login_page.login_header).to_be_visible()
+    with allure.step("Выполнить логин с корректными данными"):
+        login_page.login(email, password)
+    with allure.step(f"Проверить, что отображается 'Logged in as {username}'"):
+        expect(login_page.logged_status).to_contain_text(f'Logged in as {username}')
+        expect(login_page.logged_status).to_be_visible()
+    with allure.step("Нажать на кнопку Logout"):
+        login_page.logout_link.click()
+    with allure.step("Проверить, что пользователя вернуло на Login Page"):
+        expect(page).to_have_url("https://automationexercise.com/login")
+
+@allure.story('Authentication feature')
 @allure.title("Login User with correct email and password")
 def test_user_login_correct(login_page, acc_deleted_page):
     with allure.step("Открыть домашнюю страницу"):
@@ -75,5 +94,8 @@ def test_user_login_incorrect(login_page, acc_deleted_page):
         login_page.login("incorrect@email.ru", "password_incorrect")
     with allure.step("Проверить, что отображается сообщение о некорректных данных для входа"):
         expect(login_page.incorrect_alert).to_be_visible()
+
+
+
 
 
